7d4eaadf3b89819f18ce640b7c46533f6446bd4e
Armando del Río <adrio.coder@gmail.com>
2021-11-25 18:49:40 -0600

    add compute scale factor

--- algorithm/gauss.go
+++ algorithm/gauss.go
@@ -1,15 +1,48 @@
 package gauss
 
-func augmentedMatrix(a [][]float64, b []float64) [][]float64 {
-	// make augmented matrix
-	m := len(b)
-	ab := make([][]float64, m)
+import (
+	"errors"
+	"math"
+)
 
-	for i, ai := range a {
+func augmentedMatrix(a0 [][]float64, b0 []float64) [][]float64 {
+	// make augmented matrix
+	m := len(b0)
+	a := make([][]float64, m)
+
+	for i, ai := range a0 {
 		row := make([]float64, m+1)
 		copy(row, ai)
-		row[m] = b[i]
-		ab[i] = row
+		row[m] = b0[i]
+		a[i] = row
 	}
-	return ab
+	return a
+}
+
+func computeScaleFactor(a [][]float64, k, m int) (int, error) {
+	max := -1.0
+	iMax := 0
+
+	for i := k; i < m; i++ {
+		row := a[i]
+		// compute scale factor s = max abs in row
+		s := -1.
+		for j := k; j < m; j++ {
+			x := math.Abs(row[j])
+			if x > s {
+				s = x
+			}
+		}
+		// scale the abs used to pick the pivot.
+		if abs := math.Abs(row[k]) / s; abs > max {
+			iMax = i
+			max = abs
+		}
+	}
+
+	if a[iMax][k] == 0 {
+		return 0, errors.New("matrix is unsolvable")
+	}
+
+	return iMax, nil
 }
--- algorithm/gauss_test.go
+++ algorithm/gauss_test.go
@@ -6,12 +6,12 @@ import (
 )
 
 func TestAugmentedMatrix(t *testing.T) {
-	a := [][]float64{
+	a0 := [][]float64{
 		{1, 2, 3},
 		{2, 3, 4},
 		{3, 4, 5},
 	}
-	b := []float64{6, 7, 8}
+	b0 := []float64{6, 7, 8}
 
 	want := [][]float64{
 		{1, 2, 3, 6},
@@ -19,9 +19,28 @@ func TestAugmentedMatrix(t *testing.T) {
 		{3, 4, 5, 8},
 	}
 
-	got := augmentedMatrix(a, b)
+	got := augmentedMatrix(a0, b0)
 
 	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("\nWanted %v, \ngot %v", want, got)
+	}
+}
+
+func TestComputeScaleFactor(t *testing.T) {
+	a := [][]float64{
+		{2.11, -4.21, 0.921, 2.01},
+		{4.01, 10.2, 1.12, -3.09},
+		{1.09, .987, .832, 4.21},
+	}
+
+	want := 2
+
+	got, err := computeScaleFactor(a, 0, 3)
+	if err != nil {
+		t.Fatalf("did not expect error, but got %v", err)
+	}
+
+	if got != want {
 		t.Fatalf("\nWanted %v, \ngot %v", want, got)
 	}
 }
